d8947476721b18deb9fdc2d24dde9be7b8c61d33
Sudharshan S H <56307162+sudarshanshkrishna@users.noreply.github.com>
2023-06-19 12:34:44 +0530

    Update SettingsRowView.swift

--- UberCloneSwiftUI/UberCloneSwiftUI/ReusableItems/SettingsRowView.swift
+++ UberCloneSwiftUI/UberCloneSwiftUI/ReusableItems/SettingsRowView.swift
@@ -1,8 +1,8 @@
 //
 //  SettingsRowView.swift
-//  UberSwiftUITutorial
+//  UberCloneSwiftUI
 //
-//  Created by Stephan Dowless on 12/14/22.
+//  Created by Sudharshan S H on 03/03/22.
 //
 
 import SwiftUI
