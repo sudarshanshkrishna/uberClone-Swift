3ba4cd3adcb803e2015ba9878cfaca9bbdaf41f8
Sudharshan S H <56307162+sudarshanshkrishna@users.noreply.github.com>
2023-06-19 12:35:05 +0530

    Update UserService.swift

--- UberCloneSwiftUI/UberCloneSwiftUI/Services/UserService.swift
+++ UberCloneSwiftUI/UberCloneSwiftUI/Services/UserService.swift
@@ -1,8 +1,8 @@
 //
 //  UserService.swift
-//  UberSwiftUITutorial
+//  UberCloneSwiftUI
 //
-//  Created by Stephan Dowless on 12/16/22.
+//  Created by Sudharshan S H on 03/03/22.
 //
 
 import Firebase
