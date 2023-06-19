e9626514850476b1e05e163569e4fcdcf99a4e77
Sudharshan S H <56307162+sudarshanshkrishna@users.noreply.github.com>
2023-06-19 12:32:03 +0530

    Update Double.swift

--- UberCloneSwiftUI/UberCloneSwiftUI/Extensions/Double.swift
+++ UberCloneSwiftUI/UberCloneSwiftUI/Extensions/Double.swift
@@ -1,8 +1,8 @@
 //
 //  Double.swift
-//  UberSwiftUITutorial
+//  UberCloneSwiftUI
 //
-//  Created by Stephan Dowless on 9/14/22.
+//  Created by Sudharshan S H on 03/03/22.
 //
 
 import Foundation
