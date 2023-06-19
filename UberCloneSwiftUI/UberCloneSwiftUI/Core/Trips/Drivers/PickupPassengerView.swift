7b30abffcdf31114b2e519ece2cb8756939caa82
Sudharshan S H <56307162+sudarshanshkrishna@users.noreply.github.com>
2023-06-19 12:30:25 +0530

    Update PickupPassengerView.swift

--- UberCloneSwiftUI/UberCloneSwiftUI/Core/Trips/Drivers/PickupPassengerView.swift
+++ UberCloneSwiftUI/UberCloneSwiftUI/Core/Trips/Drivers/PickupPassengerView.swift
@@ -1,8 +1,8 @@
 //
 //  PickupPassengerView.swift
-//  UberSwiftUITutorial
+//  UberCloneSwiftUI
 //
-//  Created by Stephan Dowless on 12/22/22.
+//  Created by Sudharshan S H on 03/03/22.
 //
 
 import SwiftUI
