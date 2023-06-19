8f1592a4288b77f0c8b49f6d10987102d3489162
Sudharshan S H <56307162+sudarshanshkrishna@users.noreply.github.com>
2023-06-19 12:32:13 +0530

    Update GeoPoint.swift

--- UberCloneSwiftUI/UberCloneSwiftUI/Extensions/GeoPoint.swift
+++ UberCloneSwiftUI/UberCloneSwiftUI/Extensions/GeoPoint.swift
@@ -1,8 +1,8 @@
 //
 //  GeoPoint.swift
-//  UberSwiftUITutorial
+//  UberCloneSwiftUI
 //
-//  Created by Stephan Dowless on 12/20/22.
+//  Created by Sudharshan S H on 03/03/22.
 //
 
 import Firebase
